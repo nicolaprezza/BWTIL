60c17323a35ed60d452df006bf65b9ab4987814c
Nicola Prezza <nicolapr@gmail.com>
2014-08-14 10:41:19 +0200

    code optimizations

--- data_structures/DummyDynamicBitvector.h
+++ data_structures/DummyDynamicBitvector.h
@@ -16,19 +16,78 @@ class DummyDynamicBitvector {
 public:
 
 	DummyDynamicBitvector(){};
-	DummyDynamicBitvector(ulint n, size_t node_size=0);//allocate memory for bitvector of max length n. node size not used.
-	virtual ~DummyDynamicBitvector();
 
-	bool access(ulint i);
+	DummyDynamicBitvector(ulint n,size_t node_size) {
+
+		this->n = n;
+		bitvector = vector<bool>(n);
+		waste = vector<bool>((n*2)/10);
+		current_size = 0;
+
+	}
+
+	~DummyDynamicBitvector() {}
+
+	bool access(ulint i){
+
+		if(i>=current_size)
+			cout << "WARNING: access in position " << i << " >= current size of the bitvector (" <<  current_size << ")\n";
+
+		return bitvector.at(i);
+
+	}
+
+	void insert(ulint i, bool x){
+
+		if(i>current_size)
+			cout << "WARNING (DummyDynamicBitvector): insert in position " << i << " > current size of the bitvector (" <<  current_size << ")\n";
+
+		for(ulint j = current_size;j>i;j--)
+			bitvector.at(j) = bitvector.at(j-1);
+
+		bitvector.at(i) = x;
+
+		current_size++;
+
+		if(current_size>n)
+			cout << "WARNING (DummyDynamicBitvector): maximum size exceeded\n";
+
+
+	}
+
+	void print(){
+
+		for(ulint i=0;i<size();i++)
+			cout << bitvector.at(i);
+
+		cout << endl;
+
+	}
+
+	ulint rank(ulint i, bool x){
+
+		if(x==1)
+			return rank1(i);
+
+		return i-rank1(i);
+
+	}
+
+	uint height(){//height of the packed B-tree
+
+		uint ptr_size = ceil(log2(n+1)) +1;
+		uint d = W/ptr_size;//keys per node
+		uint b = sqrt(d);//worst-case fanout
+		uint nr_of_leafs = n/W;
+		if(b<=1) b=2;//fanout at least 2
+
+		uint h = ceil(log2(nr_of_leafs)/log2(b));
+
+		return h;
+
+	}
+
 	ulint size(){return current_size;};//current size
-
-	void insert(ulint i, bool x);
-
-	ulint rank(ulint i, bool x = true);
-
-	void print();
-
-	uint height();//height of the tree
 
 	ulint maxSize(){return n;};
 
@@ -68,7 +127,16 @@ private:
 
 	static const uint W = 512;//word length
 
-	ulint rank1(ulint i);
+	ulint rank1(ulint i){
+
+		ulint r=0;
+
+		for(ulint j=0;j<i;j++)
+			r += bitvector.at(j);
+
+		return r;
+
+	}
 
 	ulint current_size;
 	ulint n;
--- data_structures/PartialSums.h
+++ data_structures/PartialSums.h
@@ -20,17 +20,157 @@ class PartialSums {
 
 public:
 
-	PartialSums(){};
+	PartialSums(){nr_of_nodes=0;};
 
 	//the structure mantains one counter for each symbol in {0,1,...,sigma-1}.
-	PartialSums(uint sigma, ulint n);//size of the alphabet and maximum number to be stored in a counter
+	PartialSums(uint sigma, ulint n){//size of the alphabet and maximum number to be stored in a counter
 
-	void increment(symbol s);//increment by 1 the counter in s, s+1, s+2, ...
+		nr_of_nodes=0;
+
+		if(n==0){//empty counters
+			empty=true;
+			return;
+		}
+
+		base_counter = 0;
+
+		empty=false;
+		this->sigma=sigma;
+
+		log2n = ceil(log2(n+1));
+
+		uint w=64;
+		d = w/log2n;
+
+		ones = (ulint)0;
+		for(uint i=0;i<d;i++)
+			ones = ones | (((ulint)1)<<(i*log2n));
+
+		nr_of_leafs = sigma/d + (sigma%d==0?0:1);//leafs
+
+		uint nr_of_nodes_in_level = nr_of_leafs;
+		nr_of_nodes = nr_of_nodes_in_level;
+
+		//round nr_of_nodes_in_level to the next power of d+1
+
+		uint nodes_pow = 1;
+		while(nodes_pow<nr_of_nodes_in_level)
+			nodes_pow *= (d+1);
+
+		nr_of_nodes_in_level = nodes_pow;
+		uint height=1;
+
+		while(nr_of_nodes_in_level>1){
+
+			height++;
+			nr_of_nodes_in_level = nr_of_nodes_in_level/(d+1);
+			nr_of_nodes += nr_of_nodes_in_level;
+
+		}
+
+		/*cout<<"log2n="<<(uint)log2n<<endl;
+		cout << "d="<<(uint)d<<endl;
+		cout << "nr_of_leafs="<<(uint)nr_of_leafs<<endl;
+		cout << "height="<<height<<endl;
+		cout << "nr_of_nodes="<<(uint)nr_of_nodes<<endl;*/
+
+		nodes = new ulint[nr_of_nodes];
+		for(uint i=0;i<nr_of_nodes;i++)//reset all counters
+			nodes[i]=0;
+
+	}
+
+	string toString(){
+
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): toString() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+		stringstream ss;
+
+		 for(uint i =0;i<sigma;i++)
+			ss << getCount(i) << " ";
+
+		 return ss.str();
+
+	}
+
+	void increment(symbol s){
+
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): increment() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+	#ifdef DEBUG
+		if(s>=sigma){
+			cout << "ERROR (PartialSums): symbol " << s << " not in alphabet.\n";
+			exit(0);
+		}
+	#endif
+
+		uint current_node = (nr_of_nodes - nr_of_leafs) + (s/d);//offset leafs + leaf number
+		uint offset_in_node = s%d;//number of the counter inside the node
+
+		incrementFrom(&nodes[current_node], offset_in_node);//increment counters in the leaf
+
+		while(current_node>0){//repeat while current node is not the root
+
+			offset_in_node = childNumber(current_node);
+			current_node = parent(current_node);
+
+			incrementFrom(nodes+current_node, offset_in_node);
+
+		}
+
+	}
 
 	//number of symbols <s inserted. Eg: getCount(2) returns number of occurrencies of 0 and 1. getCount(0) returns always 0.
-	ulint getCount(symbol s);
+	ulint getCount(symbol s){
 
-	string toString();
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): getCount() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+		if(s==0)
+			return base_counter;
+
+		s--;
+
+	#ifdef DEBUG
+		if(s>=sigma){
+			cout << "ERROR (PartialSums): symbol " << s << " not in alphabet.\n";
+			exit(0);
+		}
+	#endif
+
+		uint current_node = (nr_of_nodes - nr_of_leafs) + (s/d);//offset leafs + leaf number
+
+		uint offset_in_node = s%d;//number of the counter inside the node
+
+		ulint count = getCounterNumber(nodes[current_node],offset_in_node);
+
+		while(current_node>0){//repeat while current node is not the root
+
+			offset_in_node = childNumber(current_node);
+			current_node = parent(current_node);
+
+			if(offset_in_node>0)
+				count += getCounterNumber(nodes[current_node],offset_in_node-1);
+
+		}
+
+		return count + base_counter;
+
+	}
 
 	void setBaseCounter(){base_counter=1;};
 
@@ -42,11 +182,111 @@ public:
 
 private:
 
-	void incrementFrom(ulint * node, uint i);//increment counters in node by 1 starting from counter number i (from left)
-	uint16_t parent(uint16_t n);//return parent of node n
-	uint16_t child(uint16_t n, uint8_t i);//return child number i of node n
-	uint8_t childNumber(uint16_t n);//return which children number is n in his parent
-	ulint getCounterNumber(ulint n, uint i);//get value of the counter number i in the node n
+	//increment counters in node by 1 starting from counter number i (from left)
+	inline void incrementFrom(ulint * node, uint i){
+
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): incrementFrom() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+	#ifdef DEBUG
+		if(i>d){
+			cout << "ERROR (PartialSums): incrementing counter i>d (" << i << ">" << d << ")\n";
+			exit(0);
+		}
+	#endif
+
+		ulint MASK = ~((ulint)0);
+
+		if(i>0) MASK = ((ulint)1<<((d-i)*log2n))-1;
+
+		//printWord(MASK&ones);
+
+		*node = *node + (MASK&ones);
+
+	}
+
+	//get value of the counter number 0<=i<d in the node n
+	inline ulint getCounterNumber(ulint n, uint i){
+
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): getCounterNumber() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+	#ifdef DEBUG
+		if(i>=d){
+			cout << "ERROR (PartialSums): get counter i>=d (" << i << ">=" << d << ")\n";
+			exit(0);
+		}
+	#endif
+
+		ulint MASK = (((ulint)1)<<log2n)-1;
+
+		return (n>>((d-i-1)*log2n))&MASK;
+
+	}
+
+	//return child number i of node n
+	inline uint16_t child(uint16_t n, uint8_t i){//return child number 0<=i<=d of node n
+
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): child() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+	#ifdef DEBUG
+		if(i>d){
+			cout << "ERROR (PartialSums): child number i>d (" << i << ">" << d << ")\n";
+			exit(0);
+		}
+	#endif
+
+		return (n*(d+1))+i+1;
+
+	}
+
+	//return parent of node n
+	inline uint16_t parent(uint16_t n){//return parent of node n
+
+	#ifdef DEBUG
+		if(empty){
+			cout << "ERROR (PartialSums): parent() called on empty counter\n";
+			exit(0);
+		}
+	#endif
+
+	#ifdef DEBUG
+		if(n>=nr_of_nodes){
+			cout << "ERROR (PartialSums): parent of inexistent node " << n <<"\n";
+			exit(0);
+		}
+	#endif
+
+		return (n-(((n-1)%(d+1))+1))/(d+1);
+
+	}
+
+	//return which children number is n in his parent
+	inline uint8_t childNumber(uint16_t n){
+
+	#ifdef DEBUG
+		if(n>=nr_of_nodes){
+			cout << "ERROR (PartialSums): parent of inexistent node " << n << "\n";
+			exit(0);
+		}
+	#endif
+
+		return (n-1)%(d+1);
+
+	}
 
 	uint8_t sigma;
 	uint8_t nr_of_leafs;
@@ -60,8 +300,6 @@ private:
 
 	ulint ones;//1^d in base 2^log2n
 	ulint * nodes;//each node is a 64-bits word and stores d counters
-
-
 
 };
 
